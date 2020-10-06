fb45ad4819770fcf7a008c53869a724dac768347
felica05-PC\felica05 <gen08350835@gmail.com>
2020-10-06 10:37:44 +0900

    commmit

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, url_for, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from instance.database import SQLALCHEMY_DATABASE_URI as DATABASE_URI
+import os
+
+app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://drzhpnvaysjdcq:2c31e06af27a1ba954c00c1e0dc9441be0d3018fe2d404421b917c8ec55e5da6@ec2-34-192-122-0.compute-1.amazonaws.com:5432/d8jgvj9lv98s4j"
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+SEACRET_KEY = os.urandom(24)
+db = SQLAlchemy(app)
+
+
+# モデル
+class Task(db.Model):
+
+  __tablename__ = "tasks"
+  id = db.Column(db.Integer, primary_key=True)
+  date = db.Column(db.String)
+  task_type = db.Column(db.String)
+  priority = db.Column(db.Integer)
+  title = db.Column(db.String)
+  text = db.Column(db.String)
+
+
+# index.html
+@app.route('/', methods=['POST','GET'])
+def index():
+  if request.method == 'POST':
+    task = Task()
+    task.date = str(datetime.today().year) + "/" + str(datetime.today().month) + "/" + str(datetime.today().day) + " " + str(datetime.today().hour) + "/" + str(datetime.today().minute)
+    task.task_type = request.form.get('task_type')
+    task.priority = request.form.get('priority')
+    task.title = request.form.get('title')
+    task.text = request.form.get('text')
+
+    try:
+      db.session.add(task)
+      db.session.commit()
+      return redirect('/')
+    except:
+      return "add failed"
+
+  else:
+    tasks = Task.query.all()
+    return render_template("index.html", tasks=tasks)
+
+
+# /delete
+@app.route('/delete/<int:id>')
+def delete(id):
+  task = Task.query.get(id)
+
+  try:
+    db.session.delete(task)
+    db.session.commit()
+    db.session.close()
+    return redirect('/')
+  except:
+    return 'There was an problem deleting that task'
+
+
+# /edit
+@app.route('/edit/<int:id>', methods=['GET'])
+def edit(id):
+  task = Task.query.get(id)
+  return render_template('edit.html', task=task)
+
+# /update
+@app.route('/update/<int:id>', methods=["GET","POST"])
+def update(id):
+    task = Task.query.get(id)
+
+    if request.method == 'POST':
+      task.date = str(datetime.today().year) + "/" + str(datetime.today().month) + "/" + str(datetime.today().day) + " " + str(datetime.today().hour) + "/" + str(datetime.today().minute) 
+      task.task_type = request.form.get('task_type')
+      task.priority = request.form.get('priority')
+      task.title = request.form.get('title')
+      task.text = request.form.get('text')
+
+      try:
+        db.session.add(task)
+        db.session.commit()
+        return redirect('/')
+      except:
+        return "edit failed"
+
+    else:
+      return render_template('edit.html',tasks=tasks)
+
+
+if __name__ == "__main__":
+  app.run()
